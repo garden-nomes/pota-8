cd42dedf023f7c0a7256d3322715228be1169d70
Naomi <naomibweiner@gmail.com>
2021-01-28 12:47:51 -0800

    add CLI for generating audio sprites

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { exec } from "child_process";
 import yargs from "yargs";
+import generateSoundSprite from "./lib/generate-sound-sprite";
 import rewriteSpriteData from "./lib/rewrite-sprite-data";
-import processSpriteData from "./lib/rewrite-sprite-data";
 
 const ASEPRITE_PATH = process.env["ASEPRITE_PATH"];
 
@@ -10,6 +10,21 @@ yargs
   .scriptName("spuds")
   .usage("$0 <cmd> [args]")
   .help()
+  .command(
+    "build-sounds <dir> [flags]",
+    "compile directory of sounds into sound sprite",
+    yargs => {
+      yargs.positional("dir", {
+        type: "string",
+        default: "./",
+        describe: "directory of audio files"
+      });
+    },
+    async argv => {
+      const data = await generateSoundSprite(argv.dir as string);
+      console.dir(data);
+    }
+  )
   .command(
     "build-aseprite <dir> [flags]",
     "build directory of .aseprite files for use with SPUDS",

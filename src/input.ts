a98eabf738201fd7554c9008c900028666ab3aef
nomes <noahbweiner@gmail.com>
2021-02-06 09:36:25 -0800

    prevent scrolling with arrow keys

--- src/input.ts
+++ src/input.ts
@@ -18,6 +18,16 @@ const keyState = new (class {
     window.addEventListener("keydown", event => {
       this.keys[event.key] = true;
       this.pressed[event.key] = true;
+
+      // prevent scrolling
+      if (
+        event.key === "ArrowUp" ||
+        event.key === "ArrowDown" ||
+        event.key === "ArrowLeft" ||
+        event.key === "ArrowRight"
+      ) {
+        event.preventDefault();
+      }
     });
 
     window.addEventListener("keyup", event => {
